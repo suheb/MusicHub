c1968dda447763fc06d0072de84742f4b0b2b0b9
Suhaib Khan <suhebjerk@gmail.com>
2014-10-25 19:42:17 +0530

    Sending file & simultaneous playback on two device with some lag.

--- app/src/main/java/com/haloappstudio/musichub/ClientActivity.java
+++ app/src/main/java/com/haloappstudio/musichub/ClientActivity.java
@@ -1,58 +1,115 @@
 package com.haloappstudio.musichub;
 
-import android.content.Context;
-import android.net.wifi.WifiManager;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
-import android.text.format.Formatter;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
-import com.haloappstudio.musichub.utils.Utils;
+import com.koushikdutta.async.ByteBufferList;
+import com.koushikdutta.async.DataEmitter;
+import com.koushikdutta.async.callback.DataCallback;
 import com.koushikdutta.async.http.AsyncHttpClient;
 import com.koushikdutta.async.http.WebSocket;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class ClientActivity extends ActionBarActivity {
     private AsyncHttpClient  mAsyncHttpClient;
-    private WifiManager mWifiManager;
     private AsyncHttpClient.WebSocketConnectCallback mWebSocketConnectCallback;
+    private WebSocket mWebSocket;
+    private MediaPlayer mMediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client);
-        mWifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-        //Resolve IP address
-        int ipAddress = mWifiManager.getConnectionInfo().getIpAddress();
-        String hostAddress = Formatter.formatIpAddress(ipAddress);
-        hostAddress = "http://" + hostAddress + ":" +Utils.PORT_NUMBER;
-        Log.d("CLIENTTAG", "address is " + hostAddress);
+
+        File dir =  new File(Environment.getExternalStorageDirectory(),"/MusicHub");
+        if(!dir.exists()) {
+            dir.mkdirs();
+        }
+        final File file = new File(dir,"temp.mp3");
+        if(file.exists()) {
+            file.delete();
+        }
+        mMediaPlayer = new MediaPlayer();
+        mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mMediaPlayer.setScreenOnWhilePlaying(true);
+        try {
+            mMediaPlayer.setDataSource(this, Uri.fromFile(file));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         mWebSocketConnectCallback = new AsyncHttpClient.WebSocketConnectCallback() {
             @Override
-            public void onCompleted(Exception ex, WebSocket webSocket) {
+            public void onCompleted(Exception ex, final WebSocket webSocket) {
                 if (ex != null) {
                     ex.printStackTrace();
                     return;
                 }
-                webSocket.send("Hello Server");
+                mWebSocket = webSocket;
+               // webSocket.send("Hello Server");
                 webSocket.setStringCallback(new WebSocket.StringCallback() {
                     @Override
                     public void onStringAvailable(final String s) {
-                        Log.d("CLIENTTAG",s);
-                        runOnUiThread(new Runnable() {
+                        /*ClientActivity.this.runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+                                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
                             }
-                        });
+                        });*/
+                        Log.d("TAG", s);
+                        if(s.contains("play")){
+                            String[] strings = s.split("-");
+                            try {
+                                mMediaPlayer.prepare();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            mMediaPlayer.start();
+                            mMediaPlayer.seekTo(Integer.parseInt(strings[1]));
+                        }
+                        if(s.contains("seek")){
+                            String[] strings = s.split("-");
+                            Log.d("TAG", "lag" + (Integer.parseInt(strings[1]) - mMediaPlayer.getCurrentPosition()));
+                            mMediaPlayer.seekTo(Integer.parseInt(strings[1]));
+                        }
+                    }
+                });
+                webSocket.setDataCallback(new DataCallback() {
+                    @Override
+                    public void onDataAvailable(DataEmitter emitter, final ByteBufferList bb) {
+                        byte[] bytes = bb.getAllByteArray();
+                        Log.d("TAG", "Got "+ bytes.length +" bytes");
+                        webSocket.ping("pong");
+                        try {
+                            FileOutputStream fileOutputStream = new FileOutputStream(file, true);
+                            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+                            bufferedOutputStream.write(bytes);
+
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        bb.recycle();
                     }
                 });
             }
         };
         mAsyncHttpClient = AsyncHttpClient.getDefaultInstance();
-        mAsyncHttpClient.websocket(hostAddress, null, mWebSocketConnectCallback);
+        mAsyncHttpClient.websocket("ws://192.168.43.1:8585", null, mWebSocketConnectCallback);
 
     }
 
@@ -74,5 +131,24 @@ public class ClientActivity extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mWebSocket!=null) {
+            mWebSocket.close();
+        }
+    }
+
+    private void setTimer() {
+        new Timer("keep_alive").scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                if (mWebSocket != null) {
+                    mWebSocket.send("random");
+                }
+            }
+        }, 2000, 3000);
     }
 }
--- app/src/main/java/com/haloappstudio/musichub/ServerActivity.java
+++ app/src/main/java/com/haloappstudio/musichub/ServerActivity.java
@@ -1,12 +1,17 @@
 package com.haloappstudio.musichub;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.haloappstudio.musichub.utils.Utils;
 import com.koushikdutta.async.AsyncServerSocket;
@@ -15,6 +20,11 @@ import com.koushikdutta.async.http.WebSocket;
 import com.koushikdutta.async.http.libcore.RequestHeaders;
 import com.koushikdutta.async.http.server.AsyncHttpServer;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,7 +33,7 @@ public class ServerActivity extends ActionBarActivity {
     private List<WebSocket> mSockets;
     private AsyncHttpServer mAsyncHttpServer;
     private AsyncHttpServer.WebSocketRequestCallback mWebSocketCallback;
-
+    private MediaPlayer mMediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,11 +41,59 @@ public class ServerActivity extends ActionBarActivity {
 
         mSockets = new ArrayList<WebSocket>();
         mAsyncHttpServer = new AsyncHttpServer();
+        mMediaPlayer = new MediaPlayer();
+
+        File file = new File(Environment.getExternalStorageDirectory(),"/Music/preview.mp3");
+        final int fileSize = (int) file.length();
+        final int chunk = 10000;
+        final int lastOffset = fileSize - (fileSize % chunk);
+        final byte[] bytes = new byte[fileSize];
+
+        try {
+            // set file for playback
+            mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            mMediaPlayer.setDataSource(this, Uri.fromFile(file));
+            mMediaPlayer.prepareAsync();
+            // read file into bytes for sending
+            BufferedInputStream buf = new BufferedInputStream(new FileInputStream(file));
+            buf.read(bytes);
+            buf.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mMediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.start();
+            }
+        });
+
+        Button sendButton = (Button) findViewById(R.id.sendButtonS);
+        sendButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                for(WebSocket socket : mSockets) {
+                    socket.send("seek-" + mMediaPlayer.getCurrentPosition());
+                }
+            }
+        });
+
         mWebSocketCallback = new AsyncHttpServer.WebSocketRequestCallback() {
             @Override
             public void onConnected(final WebSocket webSocket, RequestHeaders headers) {
+                // add to connected socket list
                 mSockets.add(webSocket);
-                webSocket.send("Welcome Client");
+                //send file in chunks
+                for(int offset = 0; offset < lastOffset; offset += chunk) {
+                    webSocket.send(bytes, offset, offset+chunk);
+                }
+                webSocket.send(bytes, lastOffset, fileSize);
+                webSocket.send("File sent");
+                webSocket.send("play-" + mMediaPlayer.getCurrentPosition());
+                //Use this to clean up any references to your websocket
                 webSocket.setClosedCallback(new CompletedCallback() {
                     @Override
                     public void onCompleted(Exception ex) {
@@ -49,8 +107,15 @@ public class ServerActivity extends ActionBarActivity {
                 });
                 webSocket.setStringCallback(new WebSocket.StringCallback() {
                     @Override
-                    public void onStringAvailable(String s) {
-                        Log.d("SERVERTAG",s);
+                    public void onStringAvailable(final String s) {
+                        ServerActivity.this.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                        if ("Hello Server".equals(s))
+                            webSocket.send("Welcome Client!");
                     }
                 });
             }
@@ -58,17 +123,6 @@ public class ServerActivity extends ActionBarActivity {
 
         mAsyncHttpServer.websocket("/", mWebSocketCallback);
         AsyncServerSocket soc = mAsyncHttpServer.listen(Utils.PORT_NUMBER);
-        Log.d("BOGUS","BOGUS");
-
-        Button sendButton = (Button) findViewById(R.id.sendButtonS);
-        sendButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                for(WebSocket socket : mSockets) {
-                    socket.send("Server sent a string");
-                }
-            }
-        });
 
     }
 
@@ -90,5 +144,12 @@ public class ServerActivity extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mAsyncHttpServer.stop();
+        mMediaPlayer.release();
     }
 }
