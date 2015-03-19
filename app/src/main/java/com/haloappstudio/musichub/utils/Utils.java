47b2bd4a7d745e15047d2c6890d5ec1fd8bc2cb9
Suhaib Khan <suhebjerk@gmail.com>
2015-03-19 20:30:56 +0530

    Fixing permissions

--- app/src/main/java/com/haloappstudio/musichub/utils/Utils.java
+++ app/src/main/java/com/haloappstudio/musichub/utils/Utils.java
@@ -9,7 +9,11 @@ import android.net.NetworkInfo;
  */
 public class Utils {
     public static final int PORT_NUMBER = 8585;
-   
+    public static final String KEY_PREPARE = "prepare";
+    public static final String KEY_SEEK = "seek";
+    public static final String KEY_FILE = "file";
+    public static final String KEY_SYNC = "sync";
+
     public static Boolean isWifiConnected(Context context) {
         ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(context.CONNECTIVITY_SERVICE);
         NetworkInfo wifiInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
